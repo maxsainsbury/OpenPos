05eec79a1d71d977256da33658d5a0a87e8d6d7b
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-05-06 11:03:54 -0700

    initialized git repository

---
+++ src/openpos/OpenPos.java
@@ -0,0 +1,16 @@
+package openpos;
+
+/**
+ *
+ * @author Max Sainsbury
+ */
+public class OpenPos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
