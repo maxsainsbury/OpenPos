78bdd313475e9000d00f49ca25d459d8b7a86ed9
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-05-06 11:44:11 -0700

    Added a full screen main frame

--- src/openpos/OpenPos.java
+++ src/openpos/OpenPos.java
@@ -1,5 +1,10 @@
 package openpos;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+import view.MainFrame;
+
 /**
  *
  * @author Max Sainsbury
@@ -10,7 +15,10 @@ public class OpenPos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        JFrame frame = new MainFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
     }
     
 }
